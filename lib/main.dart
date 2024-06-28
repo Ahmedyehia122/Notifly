2c00bb8d5c97f79b56102714c7c2fc12d8e366f1
Ahmed Yahia <ahmedyehia.122a@gmail.com>
2024-06-29 00:06:41 +0300

    provide add  note cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:note_app/core/constants/fonts.dart';
 import 'package:note_app/core/constants/routes.dart';
 import 'package:note_app/core/constants/strings.dart';
+import 'package:note_app/features/notes/data/cubits/add_notes_cubit/add_notes_cubit.dart';
 import 'package:note_app/features/notes/models/note_model.dart';
 import 'package:note_app/routes.dart';
 
@@ -26,14 +28,21 @@ class NoteApp extends StatelessWidget {
       minTextAdapt: true,
       splitScreenMode: true,
       builder: (context, child) {
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          theme: ThemeData(
-            fontFamily: AppFonts.poppnis,
-            brightness: Brightness.dark,
+        return MultiBlocProvider(
+          providers: [
+            BlocProvider(
+              create: (context) => AddNotesCubit(),
+            )
+          ],
+          child: MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              fontFamily: AppFonts.poppnis,
+              brightness: Brightness.dark,
+            ),
+            initialRoute: AppRoutes.initRoute,
+            routes: routes,
           ),
-          initialRoute: AppRoutes.initRoute,
-          routes: routes,
         );
       },
     );
