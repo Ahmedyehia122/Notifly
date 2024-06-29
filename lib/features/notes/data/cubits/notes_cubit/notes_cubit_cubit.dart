9c6ecd9a18f02cf841b7fa3ae1e2c82e25d210aa
Ahmed Yahia <ahmedyehia.122a@gmail.com>
2024-06-29 19:56:40 +0300

    create fetch note method

--- lib/features/notes/data/cubits/notes_cubit/notes_cubit_cubit.dart
+++ lib/features/notes/data/cubits/notes_cubit/notes_cubit_cubit.dart
@@ -1,9 +1,25 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
+import 'package:note_app/core/constants/strings.dart';
 import 'package:note_app/features/notes/models/note_model.dart';
 
 part 'notes_cubit_state.dart';
 
 class NotesCubitCubit extends Cubit<NotesCubitState> {
   NotesCubitCubit() : super(NotesCubitInitial());
+
+  fetchNotes() async {
+    try {
+      var noteBox = Hive.box<NoteModel>(AppStrings.kNotesBox);
+      var notesList = noteBox.values.toList();
+      emit(NotesSuccessState(notes: notesList));
+    } catch (e) {
+      emit(
+        NotesFailureState(
+          errMessage: e.toString(),
+        ),
+      );
+    }
+  }
 }
--- lib/features/notes/presentation/views/notes_view.dart
+++ lib/features/notes/presentation/views/notes_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:note_app/core/constants/colors.dart';
+import 'package:note_app/features/notes/data/cubits/notes_cubit/notes_cubit_cubit.dart';
 import 'package:note_app/features/notes/presentation/widgets/add_note_button_sheet.dart';
 import 'package:note_app/features/notes/presentation/widgets/notes_view_body.dart';
 
@@ -9,27 +11,30 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: AppColors.kSecondaryColor,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(50.r),
+    return BlocProvider(
+      create: (context) => NotesCubitCubit(),
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: AppColors.kSecondaryColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(50.r),
+          ),
+          child: Icon(
+            Icons.add,
+            color: AppColors.kPrimaryColor,
+          ),
+          onPressed: () {
+            showModalBottomSheet(
+              isScrollControlled: true,
+              context: context,
+              builder: (context) {
+                return const AddNoteButtonSheet();
+              },
+            );
+          },
         ),
-        child: Icon(
-          Icons.add,
-          color: AppColors.kPrimaryColor,
-        ),
-        onPressed: () {
-          showModalBottomSheet(
-            isScrollControlled: true,
-            context: context,
-            builder: (context) {
-              return const AddNoteButtonSheet();
-            },
-          );
-        },
+        body: const NotesViewBody(),
       ),
-      body: const NotesViewBody(),
     );
   }
 }
